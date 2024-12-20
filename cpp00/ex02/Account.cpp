189a47d11f759566afda47c905357a3d8515d49b
Kha Tieu <tieuduykha1810@gmail.com>
2024-12-20 12:48:59 +0200

    adding cpp00/ex02

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktieu <ktieu@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 12:36:10 by ktieu             #+#    #+#             */
+/*   Updated: 2024/12/20 12:48:41 by ktieu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+
+Account::Account(int deposit)
+{
+	
+}
