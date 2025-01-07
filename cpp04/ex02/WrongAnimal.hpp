282e5357801238055d40c01bbe0666899d0bbb9a
Kha Tieu <tieuduykha1810@gmail.com>
2025-01-07 15:21:50 +0200

    fixed bug cpp04

--- cpp04/ex02/WrongAnimal.hpp
+++ cpp04/ex02/WrongAnimal.hpp
@@ -6,7 +6,7 @@
 /*   By: ktieu <ktieu@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 01:20:50 by ktieu             #+#    #+#             */
-/*   Updated: 2025/01/03 12:45:26 by ktieu            ###   ########.fr       */
+/*   Updated: 2025/01/07 15:21:24 by ktieu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ class WrongAnimal
 		WrongAnimal(void);
 		WrongAnimal(const std::string&wronganimal);
 		WrongAnimal(const WrongAnimal&wronganimal);
-		virtual	~WrongAnimal(void);
+		~WrongAnimal(void);
 
 		WrongAnimal&	operator=(const WrongAnimal&);
 		
 		std::string	getType(void) const;
-		virtual	void	makeSound(void)	const;
+		void	makeSound(void)	const;
 };
